b6b87036931e2bb0aa6db06443108e43b0ae59db
TheTureyProgramist <150546608+TheTureyProgramist@users.noreply.github.com>
2026-01-17 11:36:44 +0200

    .

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# react-hooks-telephone-book
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import { useState, useEffect } from 'react';
+import './App.css';
+import styled from 'styled-components'
+class App extends Component {
+  render() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+}
+export default App;
